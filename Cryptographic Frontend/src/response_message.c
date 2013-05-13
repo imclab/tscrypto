c944a7dab65071c84aef553e18c6d00ff4e9de53
Francisco Cifuentes <francisco@niclabs.cl>
2013-05-13 17:39:40 -0400

    Agregado el manejo de response message.

--- Cryptographic Frontend/src/response_message.c
+++ Cryptographic Frontend/src/response_message.c
@@ -6,7 +6,6 @@
  */
 
 #include <stdlib.h>
-
 #include <string.h>
 
 #include <json-c/json_object.h>
@@ -14,62 +13,166 @@
 #include <json-c/json_tokener.h>
 #include "response_message.h"
 
+
 extern char* strdup(const char*);
 
-response_message* new_response_message (char const* json) 
+static int generate_key_pair_parse(json_object* obj, response_message_t* rm)
 {
-  response_message* ret = malloc(sizeof(*ret));
-  ret->return_code = NULL;
-  ret->value = NULL;
+  json_object* value;
+  if (json_object_is_type(obj, json_type_object)) {
 
-  struct json_object_iterator it;
-  struct json_object_iterator itEnd;
-  json_object* obj;
-
-  obj = json_tokener_parse(json);
-  it = json_object_iter_begin(obj);
-  itEnd = json_object_iter_end(obj);
-
-  while (!json_object_iter_equal(&it, &itEnd)) {
-    char const* key = json_object_iter_peek_name (&it);
-    json_object* val = json_object_iter_peek_value(&it);
-
-    if (strcmp("ReturnCode", key) == 0) { 
-      /* ReturnCode solo puede tener strings */
-      if (json_object_is_type(val, json_type_string))
-        ret->return_code = strdup(json_object_get_string(val));
-      else 
-        goto exit_with_errors;
-
-    } else if (strcmp("Value", key) == 0) {
-      if(json_object_is_type(val, json_type_null))
-        ret->return_code = NULL;
-      else 
-        ret->value = strdup(json_object_get_string(val));
+    if(json_object_object_get_ex(obj, "privateKeyHandler", &value)) {
+      if(json_object_is_type(value, json_type_int)) 
+        rm->private_key = json_object_get_int(value);
+      else return -1;
     }
+    else return -1;
 
-    json_object_iter_next(&it);
+    if(json_object_object_get_ex(obj, "publicKeyHandler", &value)) {
+      if(json_object_is_type(value, json_type_int))
+        rm->public_key = json_object_get_int(value);
+      else return -1;
+    }
+    else return -1;
+
+    return 0;
   }
-  
-  if (ret->return_code == NULL)
-    goto exit_with_errors;
-
-  return ret;
-
-exit_with_errors:
-  free(ret);
-  return NULL;
-  
+  else
+    return -1;
 }
 
-void del_response_message (response_message* rm)
+static int sign_init_parse(json_object* obj)
+{
+  if(json_object_is_type(obj, json_type_null))
+    return 0;
+  else 
+    return -1;
+}
+
+static int sign_parse(json_object* obj, response_message_t* rm) 
+{
+  json_object* value;
+  if (json_object_is_type(obj, json_type_object) && 
+      json_object_object_get_ex(obj, "signedData", &value)) {
+    if (json_object_is_type(value, json_type_string)) {
+      rm->signed_data = strdup(json_object_get_string(value));
+      return 0;
+    }
+  }
+
+  return -1;
+}
+
+static int find_key_parse(json_object* obj, response_message_t* rm)
+{
+  json_object* value;
+  if (json_object_is_type(obj, json_type_object) && 
+      json_object_object_get_ex(obj, "handler", &value)) {
+    if (json_object_is_type(value, json_type_int)) {
+      rm->handler = json_object_get_int(value);
+      return 0;
+    }
+  }
+
+  return -1;
+}
+
+static int get_attribute_parse(json_object* obj, response_message_t* rm)
+{
+  json_object* value;
+  if (json_object_is_type(obj, json_type_object) && 
+      json_object_object_get_ex(obj, "attributeValue", &value))
+  {
+    if (json_object_is_type(value, json_type_string)) {
+      rm->attribute = strdup(json_object_get_string(value));
+      return 0;
+    }
+  }
+
+  return -1;
+}
+
+response_message_t* new_response_message_json (char const* json, response_message_type type) 
+{
+  response_message_t* rm = malloc(sizeof(*rm));
+  if (rm != NULL) { /* Se pudo allocar la memoria */
+    rm->type = type;
+
+    json_object* obj = json_tokener_parse(json);
+
+    json_object* val;
+    if (json_object_object_get_ex(obj, "returnCode", &val)) {
+      /* ReturnCode solo puede tener strings */
+      if (json_object_is_type(val, json_type_string)) {
+        if (strcmp("OK", json_object_get_string(val)) != 0) 
+          rm->type = ERROR;
+      }
+      else goto on_error;
+    }
+
+    if (json_object_object_get_ex(obj, "value", &val)) {
+      /* Type-case */
+      switch (rm->type) {
+        case GENERATE_KEY_PAIR:
+          if (generate_key_pair_parse(val, rm) == -1)
+            goto on_error;
+          break;
+        case SIGN_INIT:
+          if (sign_init_parse(val) == -1)
+            goto on_error;
+          break;
+        case SIGN:
+          if (sign_parse(val, rm) == -1)
+            goto on_error;
+          break;
+        case FIND_KEY:
+          if (find_key_parse(val, rm) == -1)
+            goto on_error;
+          break;
+        case GET_ATTRIBUTE:
+          if (get_attribute_parse(val, rm) == -1)
+            goto on_error;
+          break;
+        case ERROR:
+          if (json_object_is_type(val, json_type_string)) 
+            rm->error_message = strdup(json_object_get_string(val));
+          break; 
+        default:
+          rm->error_message = strdup("Error en la llamada.");
+      }
+
+    }
+
+    json_object_put(obj);
+  }
+
+  return rm;
+
+on_error:
+  free(rm);
+  return NULL;
+}
+
+void free_response_message (response_message_t* rm)
 {
   if (rm != NULL) {
-    free(rm->return_code);
-    free(rm->value);
+    if (rm->type == GENERATE_KEY_PAIR) {
+    }
+    else if (rm->type == SIGN_INIT) { 
+    }
+    else if (rm->type == SIGN) {
+      free(rm->signed_data);
+    }
+    else if (rm->type == FIND_KEY) {
+    }
+    else if (rm->type == GET_ATTRIBUTE) {
+      free(rm->attribute);
+    }
+    else if (rm->type == ERROR) {
+      free(rm->error_message);
+    }
   }
 
   free(rm);
 }
-
 
--- Cryptographic Frontend/src/response_message.h
+++ Cryptographic Frontend/src/response_message.h
@@ -7,16 +7,33 @@
 
 #ifndef RESPONSE_MESSAGE_H_
 #define RESPONSE_MESSAGE_H_
+#include <stdbool.h>
 
+typedef enum { 
+  GENERATE_KEY_PAIR, 
+  SIGN_INIT, 
+  SIGN, 
+  FIND_KEY, 
+  GET_ATTRIBUTE, 
+  ERROR 
+} response_message_type;
 
-struct response_message {
-	char* return_code;
-	char* value;
-};
-typedef struct response_message response_message;
+typedef struct response_message {
+  response_message_type type;
+  union {
+    struct { /* GENERATE KEY PAIR */ 
+      int private_key;
+      int public_key;
+    };
+    /* SIGN INIT (NADA XD) */
+    char* signed_data; /* SIGN */
+    int handler; /* FIND KEY MESSAGE */
+    char* attribute; /* GET ATTRIBUTE */
+    char* error_message; /* ERROR */
+  };
+} response_message_t;
 
-response_message* new_response_message (char const* json);
-void del_response_message (response_message* rm);
-
+response_message_t* new_response_message (char const* json, response_message_type type);
+void response_message_free (response_message_t* rm);
 
 #endif /* RESPONSE_MESSAGE_H_ */
