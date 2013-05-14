48e5090bd85ecbec3eb480ff0c65a3b41bf3cd93
Francisco Cifuentes <francisco@niclabs.cl>
2013-05-13 23:00:04 -0400

    Modificado el main para mas claridad de uso

--- Cryptographic Frontend/src/main.c
+++ Cryptographic Frontend/src/main.c
@@ -12,11 +12,8 @@
 
 int main (int argc, char **argv) {
 	arg_list* args = new_arg_list();
-    arg_list_add_s(&args, "Hola", "Mundo");
-	arg_list_add_i(&args, "Chao", 1);
-	arg_list_add_s(&args, "Loco", "Loco");
-	arg_list_add_s(&args, "Perro", "Loco");
-	arg_list_add_i(&args, "Gato", 2);
+	arg_list_add_i(&args, "KeySize", 1024);
+	arg_list_add_i(&args, "Exponent", 65537);
 	method_message* msg = new_method_message("GenerateKeyPair", args);
 	char const* json = method_message_serialize(msg);
 	printf("%s\n", json);
