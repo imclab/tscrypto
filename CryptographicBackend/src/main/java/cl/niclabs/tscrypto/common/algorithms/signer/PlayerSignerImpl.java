2a38a45dc5f972132463a53e516afb0e2e378981
Francisco Montoto M <fmontotomonroy@gmail.com>
2014-01-27 14:55:15 -0300

    Refactoring a player signer, agregado AbstractPlayerSigner

--- CryptographicBackend/src/main/java/cl/niclabs/tscrypto/common/algorithms/signer/PlayerSignerImpl.java
+++ CryptographicBackend/src/main/java/cl/niclabs/tscrypto/common/algorithms/signer/PlayerSignerImpl.java
@@ -21,14 +21,9 @@ package cl.niclabs.tscrypto.common.algorithms.signer;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Random;
-
 import cl.niclabs.tscrypto.common.datatypes.KeyShareInfo;
 import cl.niclabs.tscrypto.common.datatypes.SignatureShare;
-import cl.niclabs.tscrypto.common.datatypes.TSPublicKey;
 import cl.niclabs.tscrypto.common.utils.TSLogger;
-import cl.niclabs.tscrypto.common.utils.ThreshUtil;
 
 /**
  * A Secret Key Share for an RSA (k,l) Threshold Scheme
@@ -42,82 +37,21 @@ import cl.niclabs.tscrypto.common.utils.ThreshUtil;
  * @author Steve Weis <sweis@mit.edu>
  * @author acansado
  */
-public class PlayerSignerImpl implements PlayerSigner {
-	/** player id */
-	private int id;
+public class PlayerSignerImpl extends AbstractPlayerSigner {
 
-	/** l! the factorial of the number of players */
-	private BigInteger delta;
-
-	/** The share hold to this player */
-	private BigInteger secretShare;
-
-	/** Public key information (includes verifiers) */
-	private TSPublicKey publicKey;
-
-	private static SecureRandom random;
-	private MessageDigest md;
-
-	/** lock to be used with Message Digest since it is not thread-safe */
-	private static Object lockMd = new Object();
-
-	static {
-		final byte[] randSeed = new byte[20];
-		(new Random()).nextBytes(randSeed);
-		random = new SecureRandom(randSeed);
-	}
-
-        /**
-         * Player signer in charge of a keyShare
-         * @param key key used by this player
-         * @param id player id
-         */
+    /**
+     * Player signer in charge of a keyShare
+     * @param key key used by this player
+     * @param id player id
+     */
 	public PlayerSignerImpl(KeyShareInfo key, int id) {
-		
-		TSLogger.node.info("Creating player " + id);
-		TSLogger.node.debug(key.toJson(true));
-
-		this.secretShare = key.getShare();
-		this.publicKey = key.getPublicKey();
-		this.id = id;
-
-		this.delta = ThreshUtil.factorial(key.getKeyMetaInfo().getL());
-		// this.e_prime = ThreshUtil.FOUR.multiply(delta).multiply(secretShare);
-	}
-
-	// Public Methods
-	// ............................................................................
-
-	/* (non-Javadoc)
-	 * @see PlayerSigner#getId()
-	 */
-	@Override
-	public int getId() {
-		return id;
-	}
-
-	/* (non-Javadoc)
-	 * @see PlayerSigner#sign(java.math.BigInteger)
-	 */
-	@Override
-	public SignatureShare sign(BigInteger document) throws NoSuchAlgorithmException {
-
-		// r \elt (0, 2^L(n)+3*l1)
-
-		final int randbits = publicKey.getModulus().bitLength() + 3 * ThreshUtil.L1;
-		final BigInteger r = (new BigInteger(randbits, random));
-
-        return sign(document, r);
-
+		super (key, id);
 	}
 
 
-	private SignatureShare sign(BigInteger document, BigInteger r) throws NoSuchAlgorithmException {
-        final BigInteger TWO = ThreshUtil.TWO;
-        final BigInteger FOUR = ThreshUtil.FOUR;
+	protected SignatureShare sign(BigInteger document, BigInteger r) throws NoSuchAlgorithmException {
 
         long initTime = System.currentTimeMillis();
-
 		BigInteger groupVerifier = publicKey.getGroupVerifier();
 		BigInteger shareVerifier = publicKey.getShareVerifier(id);
 		
--- CryptographicBackend/src/main/java/cl/niclabs/tscrypto/common/algorithms/signer/PlayerSignerJniImpl.java
+++ CryptographicBackend/src/main/java/cl/niclabs/tscrypto/common/algorithms/signer/PlayerSignerJniImpl.java
@@ -3,72 +3,24 @@ package cl.niclabs.tscrypto.common.algorithms.signer;
 import cl.niclabs.tscrypto.common.algorithms.JniSignWrapper;
 import cl.niclabs.tscrypto.common.datatypes.KeyShareInfo;
 import cl.niclabs.tscrypto.common.datatypes.SignatureShare;
-import cl.niclabs.tscrypto.common.datatypes.TSPublicKey;
 import cl.niclabs.tscrypto.common.utils.TSLogger;
-import cl.niclabs.tscrypto.common.utils.ThreshUtil;
-
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Random;
 
-public class PlayerSignerJniImpl implements PlayerSigner {
-    public final static BigInteger TWO = ThreshUtil.TWO;
-    public final static BigInteger FOUR = ThreshUtil.FOUR;
 
-    /** player id */
-    private int id;
-
-    /** l! the factorial of the number of players */
-    private BigInteger delta;
-
-    /** The share hold to this player */
-    private BigInteger secretShare;
-
-    /** Public key information (includes verifiers) */
-    private TSPublicKey publicKey;
-
-    private static SecureRandom random;
-    private MessageDigest md;
-
-    /** lock to be used with Message Digest since it is not thread-safe */
-    private static Object lockMd = new Object();
-
-    static {
-        final byte[] randSeed = new byte[20];
-        (new Random()).nextBytes(randSeed);
-        random = new SecureRandom(randSeed);
-    }
-
+public class PlayerSignerJniImpl extends AbstractPlayerSigner {
     /**
      * Player signer in charge of a keyShare
      *
      * @param key key used by this player
      * @param id  player id
      */
-    public PlayerSignerJniImpl(KeyShareInfo key, int id) {
-        this.secretShare = key.getShare();
-        this.publicKey = key.getPublicKey();
-        this.id = id;
+   public PlayerSignerJniImpl (KeyShareInfo key, int id){
+       super(key, id);
+   }
 
-        this.delta = ThreshUtil.factorial(key.getKeyMetaInfo().getL());
-    }
-
-    @Override
-    public int getId() {
-        return 0;
-    }
-
-    @Override
-    public SignatureShare sign(BigInteger document) throws NoSuchAlgorithmException {
-        final int randbits = publicKey.getModulus().bitLength() + 3 * ThreshUtil.L1;
-        final BigInteger r = (new BigInteger(randbits, random));
-
-        return jniSign(document, r);
-    }
-
-    private SignatureShare jniSign(BigInteger document, BigInteger r)throws NoSuchAlgorithmException{
+    protected SignatureShare sign(BigInteger document, BigInteger r)throws NoSuchAlgorithmException{
         long initTime = System.currentTimeMillis();
         TSLogger.node.debug("Estoy usando JNI");
         final BigInteger groupVerifier = publicKey.getGroupVerifier();
