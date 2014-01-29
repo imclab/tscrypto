d98bef67a30b1d47b5e73c0b5483b6e4d0c62e06
Francisco Cifuentes <francisco@niclabs.cl>
2014-01-29 12:44:42 -0300

    Node with threads...

--- CryptographicBackend/src/main/java/cl/niclabs/tscrypto/node/PlayerSignerFactory.java
+++ CryptographicBackend/src/main/java/cl/niclabs/tscrypto/node/PlayerSignerFactory.java
@@ -3,13 +3,21 @@ package cl.niclabs.tscrypto.node;
 import cl.niclabs.tscrypto.common.algorithms.signer.PlayerSigner;
 import cl.niclabs.tscrypto.common.algorithms.signer.PlayerSignerImpl;
 import cl.niclabs.tscrypto.common.algorithms.signer.PlayerSignerJniImpl;
+import cl.niclabs.tscrypto.common.algorithms.signer.PlayerSignerJniThreadsImpl;
 import cl.niclabs.tscrypto.common.datatypes.KeyShareInfo;
 
 public class PlayerSignerFactory {
     static public PlayerSigner getInstance(KeyShareInfo keyShareInfo, int id) {
         NodeConfig config = NodeConfig.getInstance();
-        if (config.getUseJNI()) {
-            return new PlayerSignerJniImpl(keyShareInfo, id);
+        boolean useJNI = config.getUseJNI();
+        int numThreads = config.getNumThreads();
+
+        if (useJNI) {
+            if(numThreads > 1) {
+                return new PlayerSignerJniThreadsImpl(keyShareInfo, id, numThreads);
+            } else {
+                return new PlayerSignerJniImpl(keyShareInfo, id);
+            }
         } else {
             return new PlayerSignerImpl(keyShareInfo, id);
         }
